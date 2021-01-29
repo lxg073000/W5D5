3146f213146060dc62ddc663302fde8f27822271
App Academy Student <lxg073000@gmail.com>
2021-01-29 14:15:03 -0500

    Refactor #harrison_ford

--- movie_buff/movie_buff/01_queries.rb
+++ movie_buff/movie_buff/01_queries.rb
@@ -7,6 +7,10 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
 
+  Movie
+  .select(:id, :title, :score)
+  .where(score: 2..3)
+
 end
 
 def harrison_ford
@@ -20,6 +24,11 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+  Movie
+  .select(:id, :title)
+  .joins(:actors, :castings)
+  .where("actors.name = \'Harrison Ford\' and castings.ord != 1")
+
 
 end
 
